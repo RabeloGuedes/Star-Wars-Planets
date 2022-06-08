88eaf763a62d1d84214d0361e45d2eaaf1433ab0
RabeloGuedes <krepscredian@gmail.com>
2022-06-08 18:52:37 -0300

    Cria mais estados (filters, columnFilter, comparisonFilter e comparisonNumber), adiciona multiplos filtros ao useEffect de filtragem e no 'Form.js' adiciona a tabela e seus respectivos valores.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ function App() {
   const [planets, setPlanets] = useState([]);
   const [planetNameInput, setPlanetName] = useState('');
   const [filteredPlanets, setFilteredPlanets] = useState([]);
+  const [filters, setFilters] = useState([]);
+  const [columnFilter, setColumnFilter] = useState('population');
+  const [comparisonFilter, setComparisonFilter] = useState('maior que');
+  const [comparisonNumber, setComparisonNumber] = useState(0);
   useEffect(() => {
     const apiRequest = async () => {
       const apiResponse = await fetch('https://swapi-trybe.herokuapp.com/api/planets/');
@@ -21,18 +25,45 @@ function App() {
   useEffect(() => {
     const newFilteredPlanets = planets
       .filter(({ name }) => (name.toLowerCase().includes(planetNameInput)));
-    setFilteredPlanets(newFilteredPlanets);
-  }, [planets, planetNameInput]);
 
-  const changePlanetName = ({ target: { value } }) => {
-    setPlanetName(value);
+    const multipleFilters = filters.reduce((acc, filter) => (
+      acc.filter((planet) => {
+        switch (filter.comparisonFilter) {
+        case 'maior que':
+          return planet[filter.columnFilter] > Number(filter.comparisonNumber);
+        case 'menor que':
+          return planet[filter.columnFilter] < Number(filter.comparisonNumber);
+        case 'igual a':
+          return planet[filter.columnFilter] === filter.comparisonNumber;
+        default: return true;
+        }
+      })), newFilteredPlanets);
+
+    setFilteredPlanets(multipleFilters);
+  }, [planets, planetNameInput, filters]);
+
+  const handleNumericFilters = () => {
+    const newFilter = {
+      columnFilter,
+      comparisonFilter,
+      comparisonNumber,
+    };
+    setFilters([...filters, newFilter]);
   };
 
   return (
     <div>
       <Form
-        changePlanetName={ changePlanetName }
+        setPlanetName={ setPlanetName }
         planetNameInput={ planetNameInput }
+        setColumnFilter={ setColumnFilter }
+        columnFilter={ columnFilter }
+        setComparisonFilter={ setComparisonFilter }
+        comparisonFilter={ comparisonFilter }
+        setComparisonNumber={ setComparisonNumber }
+        comparisonNumber={ comparisonNumber }
+        handleNumericFilters={ handleNumericFilters }
+        filters={ filters }
       />
       <Table planets={ filteredPlanets } />
     </div>
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,7 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default function Form(props) {
-  const { planetNameInput, changePlanetName } = props;
+  const {
+    planetNameInput,
+    setPlanetName,
+    setColumnFilter,
+    columnFilter,
+    setComparisonFilter,
+    comparisonFilter,
+    setComparisonNumber,
+    comparisonNumber,
+    handleNumericFilters,
+  } = props;
   return (
     <div>
       <input
@@ -10,13 +20,69 @@ export default function Form(props) {
         type="text"
         placeholder="Planet Name"
         value={ planetNameInput }
-        onChange={ changePlanetName }
+        onChange={ ({ target: { value } }) => setPlanetName(value) }
       />
+      <select
+        data-testid="column-filter"
+        value={ columnFilter }
+        onChange={ ({ target: { value } }) => setColumnFilter(value) }
+      >
+        <option value="population">
+          population
+        </option>
+        <option value="orbital_period">
+          orbital_period
+        </option>
+        <option value="diameter">
+          diameter
+        </option>
+        <option value="rotation_period">
+          rotation_period
+        </option>
+        <option value="surface_water">
+          surface_water
+        </option>
+      </select>
+      <select
+        data-testid="comparison-filter"
+        value={ comparisonFilter }
+        onChange={ ({ target: { value } }) => setComparisonFilter(value) }
+      >
+        <option value="maior que">
+          maior que
+        </option>
+        <option value="menor que">
+          menor que
+        </option>
+        <option value="igual a">
+          igual a
+        </option>
+      </select>
+      <input
+        type="number"
+        placeholder="0"
+        data-testid="value-filter"
+        value={ comparisonNumber }
+        onChange={ ({ target: { value } }) => setComparisonNumber(value) }
+      />
+      <button
+        type="button"
+        data-testid="button-filter"
+        onClick={ handleNumericFilters }
+      >
+        Enviar
+      </button>
     </div>
   );
 }
 
 Form.propTypes = {
   planetNameInput: PropTypes.string,
-  changePlanetName: PropTypes.func,
+  columnFilter: PropTypes.string,
+  comparisonFilter: PropTypes.string,
+  comparisonNumber: PropTypes.string,
+  setPlanetName: PropTypes.func,
+  setColumnFilter: PropTypes.func,
+  setComparisonFilter: PropTypes.func,
+  setComparisonNumber: PropTypes.func,
 }.isRequired;
