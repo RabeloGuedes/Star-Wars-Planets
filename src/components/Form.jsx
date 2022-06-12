1ce62264aba6b41fd767ea05958db824e71e75f0
RabeloGuedes <krepscredian@gmail.com>
2022-06-12 17:41:28 -0300

    Modifica a estrutura do 'context', cria mais estados, desenvolve a lógica para usar multiplos filtros.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,10 +6,34 @@ export default function Form() {
     search: {
       filterByName: {
         name,
+        setPlanetName,
       },
+      filterByColumn: {
+        columnOption,
+        comparisonOption,
+        inputValue,
+        setColumnOption,
+        setComparisonOption,
+        setValue,
+      },
+      filters,
+      setFilters,
+      columnOptions,
+      setColumnOptions,
     },
-    setPlanetName,
+    comparisonOptions,
   } = useContext(PlanetsContext);
+
+  const handleNumericFilter = () => {
+    const newFilter = {
+      columnOption,
+      comparisonOption,
+      inputValue,
+    };
+    setFilters([...filters, newFilter]);
+    const newColumnOptions = columnOptions.filter((filter) => filter !== columnOption);
+    setColumnOptions(newColumnOptions);
+  };
   return (
     <form>
       <input
@@ -19,6 +43,62 @@ export default function Form() {
         placeholder="Planet Name"
         onChange={ ({ target: { value } }) => setPlanetName(value) }
       />
+      <select
+        data-testid="column-filter"
+        value={ columnOption }
+        onChange={ ({ target: { value } }) => setColumnOption(value) }
+      >
+        {columnOptions.map((filter) => (
+          <option
+            key={ Math.random() }
+            value={ filter }
+          >
+            {filter}
+          </option>
+        ))}
+      </select>
+      <select
+        data-testid="comparison-filter"
+        value={ comparisonOption }
+        onChange={ ({ target: { value } }) => setComparisonOption(value) }
+      >
+        {Object.values(comparisonOptions).map((comparison) => (
+          <option
+            key={ Math.random() }
+            value={ comparison }
+          >
+            {comparison}
+          </option>
+        ))}
+      </select>
+      <input
+        data-testid="value-filter"
+        type="number"
+        value={ inputValue }
+        onChange={ ({ target: { value } }) => setValue(value) }
+      />
+      <button
+        type="button"
+        data-testid="button-filter"
+        onClick={ handleNumericFilter }
+      >
+        Filtrar
+      </button>
+      {filters.map((filter) => (
+        <p key={ Math.random() }>
+          {filter.columnOption}
+          {' '}
+          {filter.comparisonOption}
+          {' '}
+          {filter.inputValue}
+          {' '}
+          <button
+            type="button"
+          >
+            Deletar
+          </button>
+        </p>
+      ))}
     </form>
   );
 }
--- src/services/Provider.jsx
+++ src/services/Provider.jsx
@@ -10,12 +10,10 @@ export default function Provider({ children }) {
       comparison: 'maior que',
       value: 0,
     },
-    columnOptions: {
-      population: 'population',
-      orbital_period: 'orbital_period',
-      diameter: 'diameter',
-      rotation_period: 'rotation_period',
-      surface_water: 'surface_water',
+    comparisonOptions: {
+      maior_que: 'maior que',
+      menor_que: 'menor que',
+      igual_a: 'igual a',
     },
     categories: {
       name: 'name',
@@ -36,16 +34,39 @@ export default function Provider({ children }) {
   const [planets, setPlanets] = useState([]);
   const [planetName, setPlanetName] = useState('');
   const [filteredPlanets, setfilteredPlanets] = useState([]);
+  const [columnOption, setColumnOption] = useState('population');
+  const [comparisonOption, setComparisonOption] = useState('maior que');
+  const [inputValue, setValue] = useState(0);
+  const [filters, setFilters] = useState([]);
+  const [columnOptions, setColumnOptions] = useState([
+    'population',
+    'orbital_period',
+    'diameter',
+    'rotation_period',
+    'surface_water',
+  ]);
 
   const search = {
     filterByName: {
       name: planetName,
+      setPlanetName,
     },
+    filterByColumn: {
+      comparisonOption,
+      columnOption,
+      inputValue,
+      setColumnOption,
+      setComparisonOption,
+      setValue,
+    },
+    filters,
+    setFilters,
+    columnOptions,
+    setColumnOptions,
   };
 
   context.search = search;
   context.filteredPlanets = filteredPlanets;
-  context.setPlanetName = setPlanetName;
   context.planets = planets;
 
   useEffect(() => {
@@ -60,8 +81,23 @@ export default function Provider({ children }) {
   useEffect(() => {
     const newFilteredPlanets = planets
       .filter(({ name }) => name.toLowerCase().includes(planetName));
-    setfilteredPlanets(newFilteredPlanets);
-  }, [planetName, planets]);
+
+    const multipleFilters = filters.reduce((acc, currFilter) => (
+      acc.filter((planet) => {
+        switch (currFilter.comparisonOption) {
+        case 'maior que':
+          return planet[currFilter.columnOption] > Number(currFilter.inputValue);
+        case 'menor que':
+          return planet[currFilter.columnOption] < Number(currFilter.inputValue);
+        case 'igual a':
+          return planet[currFilter.columnOption] === currFilter.inputValue;
+        default: return true;
+        }
+      })
+    ), newFilteredPlanets);
+
+    setfilteredPlanets(multipleFilters);
+  }, [planetName, planets, filters]);
 
   return (
     <PlanetsContext.Provider value={ context }>
